1781eadc76d4ebb73985fdc3724393f5eaf5077f
hgoya <z51.hidekazu.goya@gmail.com>
2019-05-15 12:13:40 +0900

    fix1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
-  
+
+  def index
+    if params[:group_id].present?
+      @group = Group.find(params[:group_id])
+      @ids = @group.users.ids
+      @users = User.where.not(id: @ids).where('(name LIKE(?)) and (id != ?)', "%#{params[:keyword]}%", "#{current_user.id}")
+    else
+      @users = User.where('(name LIKE(?)) and (id != ?)', "%#{params[:keyword]}%", "#{current_user.id}")
+    end
+    respond_to do |format|
+      format.json
+    end
+  end
+
   def edit
   end
 
@@ -16,4 +29,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email)
   end
+
 end
