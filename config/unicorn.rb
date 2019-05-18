433ccf89ad9835c2b6a171c6239d21ec2a0b8c7a
hgoya <z51.hidekazu.goya@gmail.com>
2019-05-18 14:11:51 +0900

    fix

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,11 +1,9 @@
-# app_path = File.expand_path('../../../', __FILE__)
+app_path = File.expand_path('../../../', __FILE__)
 
-# worker_processes 1
+worker_processes 1
 
-app_path = File.expand_path('../../', __FILE__)
-ENV['BUNDLE_GEMFILE'] = app_path + "/Gemfile"
-working_directory app_path
-# working_directory "#{app_path}/current"
+
+working_directory "#{app_path}/current"
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
