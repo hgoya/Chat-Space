5b8cde3de49cc1fe47dd0334e14104d1a8347860
hgoya <z51.hidekazu.goya@gmail.com>
2019-05-23 14:49:17 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,8 @@
 app_path = File.expand_path('../../../', __FILE__)
 
-worker_processes 1
+ENV['BUNDLE_GEMFILE'] = rails_root + "/Gemfile"
+
+worker_processes 2
 
 working_directory "#{app_path}/current"
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
